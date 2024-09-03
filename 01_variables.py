661f2ea0c07e85e5dbc313b7fa45e92e9eee4c72
Alberto Cabrera <132774942+albecabrera@users.noreply.github.com>
2024-09-03 20:49:31 +0000

    otras variables

--- 01_variables.py
+++ 01_variables.py
@@ -12,5 +12,14 @@ print(type(my_int_to_str_variable))
 
 my_bool_variable = False
 print(my_bool_variable)
+# concatenación de variables en un print
+print(type(print("Mi cadena de texto")))  # tipo "NoneType"
+print("Este es el valor de:", my_bool_variable)
 
-print(my_string_variable, str(my_int_variable), my_bool_variable)
+# Algunas funciones del sistema
+print(len(my_string_variable))
+
+# Variables en una sola línea. ¡XCuidado con abusar de esta sintaxis!
+name, surname, alias, age = "Alberto", "Cabrera", "AlbeCabrera", 35
+print("Me llamo:", name, surname, ". Mi edad es:", age, "Y mi alias es:", alias)
+
