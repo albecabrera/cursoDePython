847b44e8d87c2527fb43396b7e02152934f2eab9
albecabrera <albe.cabrera@yahoo.de>
2024-09-06 12:17:57 +0200

    first commit

---
+++ 03_strings.py
@@ -0,0 +1,19 @@
+### Stirngs ###
+
+my_string = "Mi String"
+my_other_string = 'Mi otro String'
+
+print(len(my_string))
+print(len(my_other_string))
+
+print(my_string + " " + my_other_string)
+
+my_new_line_string = "Este es un String \n con salto de línea"
+print(my_new_line_string)
+
+my_tab_string = "\tEste es un String con tabulación"
+print(my_tab_string)
+
+my_scape_string = "\\tEste es un String \\n escapado"
+print(my_scape_string)
+
