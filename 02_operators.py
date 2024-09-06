aeaab38eb26889a1eb50ac41e43e30ba4f89682a
albecabrera <albe.cabrera@yahoo.de>
2024-09-06 21:41:22 +0200

    first commit

--- 02_operators.py
+++ 02_operators.py
@@ -1,4 +1,4 @@
-### Operadores ###
+### Operadores artiméticos ###
 
 print(3 + 4)  # operación de suma
 print(3 - 4)  # operación de resta
@@ -11,6 +11,19 @@ print(2**3) # operación 2 elevado a la 3
 print("Hola " + "Python " + "¿Qué tal?")
 print("Hola " + str(5))
 print("Hola " * 5)
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+### Operadores comparativos ###
+
+print("Hola" > "Pyhton")
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+
 
 
 
