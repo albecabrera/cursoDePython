443b794209f03bfca05b6afef332d3f1d120ba9e
Alberto Cabrera <132774942+albecabrera@users.noreply.github.com>
2024-09-08 07:32:17 +0000

    funciones

--- 03_strings.py
+++ 03_strings.py
@@ -52,4 +52,16 @@ print(language_slice)
 reversed_language = language[::-1]
 print(reversed_language)
 
+# Funciones
+
+print(language.capitalize())
+print(language.upper())
+print(language.lower())
+print(language.count("t"))
+print(language.isnumeric())
+print("1".isnumeric())
+print(language.lower().isupper())
+
+
+
 
