a29fe6ef940d38ab76317f57849423b068ee8279
Alberto Cabrera <132774942+albecabrera@users.noreply.github.com>
2024-09-08 20:37:20 +0000

    Lists

--- 04_lists.py
+++ 04_lists.py
@@ -1,7 +1,13 @@
 ### Lists ###
 
 my_list = list()
-my_order_list = []
+my_order_list = [35, 1.77, "Alberto", "Cabrera"]
 
 print(my_list)
 print(my_order_list)
+
+print(my_order_list[0])
+print(my_order_list[1])
+print(my_order_list[-1])
+print(my_order_list[-4])
+
