1f08749c836b19987ec2f5448be8caa0eaf8eace
Alberto Cabrera <132774942+albecabrera@users.noreply.github.com>
2024-09-08 07:22:48 +0000

    Reverse

--- 03_strings.py
+++ 03_strings.py
@@ -36,6 +36,20 @@ print(a)
 print(e)
 
 
+# División
 
+language_slice = language[1:3]
+print(language_slice)
+
+language_slice = language[1:]
+print(language_slice)
+
+language_slice = language[-2]
+print(language_slice)
+
+# Reverse
+
+reversed_language = language[::-1]
+print(reversed_language)
 
 
